4576d3d825726ff082dfbc789a23f11318653fd6
gabrieloliveira098@outlook.com <gabrieloliveira098@outlook.com>
2024-05-06 19:45:14 -0300

    projeto upload S3 AWS

---
+++ src/main/java/aprendendoS3/aws_s3_file_uploud/FileStorageServiceApplication.java
@@ -0,0 +1,13 @@
+package aprendendoS3.aws_s3_file_uploud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileStorageServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileStorageServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/aprendendoS3/aws_s3_file_uploud/FileStorageServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package aprendendoS3.aws_s3_file_uploud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileStorageServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
